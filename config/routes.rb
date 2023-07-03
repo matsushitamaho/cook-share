9a1fdf8a57b4560b3b54c9954f9386815f2f1ff6
mathushitamaho <mapo.maho.nissy0610@gmail.com>
2023-07-03 16:22:20 +0000

    [Add]ログイン機能実装(会員/新規登録・ログイン、管理者/ログイン)

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,33 @@
 Rails.application.routes.draw do
-  namespace :admin do
-    get 'post_comments/index'
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'homes/top'
-  end
-  namespace :public do
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/likes'
-    get 'recipes/index'
-    get 'recipes/new'
-    get 'recipes/show'
-    get 'recipes/edit'
-    get 'homes/top'
-    get 'homes/about'
-  end
-  # 顧客用
-  # URL /customers/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  
   devise_for :customers,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
   
-  # 管理者用
-  # URL /admin/sign_in ...
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-    sessions: "admin/sessions"
-  }
+  namespace :admin do
+    root to: "homes#top"
+    resources :customers,     only: [:index, :show, :edit, :update]
+    resources :post_comments, only: [:index, :destroy]
+  end
+  
+  
+  scope module: 'public' do
+    root to: "homes#top"
+    get "/about" => "homes#about"
+    resources :recipes,         only: [:index, :new, :create, :show, :edit, :update] do
+      resource :favorites,      only: [:create, :destroy]
+      resources :post_comments, only: [:new, :create, :destroy]
+    end
+    resource :customer, only: [:show, :edit, :update] do
+      get 'like',         on: :collection
+      get 'leave',        on: :collection
+      patch 'withdwaral', on: :collection
+    end
+  end
   
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

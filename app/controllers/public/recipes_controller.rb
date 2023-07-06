552448695af1f8b6bc79c0b4c8ed305528d97280
mathushitamaho <mapo.maho.nissy0610@gmail.com>
2023-07-06 15:09:32 +0000

    [Add]レシピ新規投稿機能

--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -1,8 +1,11 @@
 class Public::RecipesController < ApplicationController
+  before_action :authenticate_customer!, only:  [:new, :edit, :create, :update]
+
   def index
   end
 
   def new
+    @recipe = Recipe.new
   end
 
   def show
@@ -10,10 +13,17 @@ class Public::RecipesController < ApplicationController
 
   def edit
   end
-  
+
   def create
+    @recipe = Recipe.new(recipe_params)
   end
-  
+
   def update
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :time, :material, :make)
   end
 end

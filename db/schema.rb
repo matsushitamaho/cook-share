cfab21fe1f7ed8a2a0ef96c50f16e049f7389724
mathushitamaho <mapo.maho.nissy0610@gmail.com>
2023-07-11 02:09:20 +0000

    miss

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_08_053111) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_11_020403) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -81,6 +81,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_08_053111) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "post_tags", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "tag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_tags_on_post_id"
+    t.index ["tag_id"], name: "index_post_tags_on_tag_id"
+  end
+
   create_table "recipes", force: :cascade do |t|
     t.integer "customer_id", null: false
     t.string "name", null: false
@@ -91,6 +100,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_08_053111) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "post_tags", "posts"
+  add_foreign_key "post_tags", "tags"
 end

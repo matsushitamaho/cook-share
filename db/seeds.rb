0c8196217f8e4aa30918bb4caecc342b3b689a8a
mathushitamaho <mapo.maho.nissy0610@gmail.com>
2023-07-17 05:07:47 +0000

    seedsに会員情報追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,15 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 Admin.create!(
-   email: 'maho@hoge.com',
-   password: 'testmaho',
+   email: 'test@hoge.com',
+   password: 'testhoge',
 )
 
 Tag.create([
    { name: 'サラダ'},
    { name: 'ごはんもの'},
    { name: 'グラタン・ドリア'},
-   { name: 'パスタ'},
+   { name: '麺類'},
    { name: '漬物・佃煮'},
    { name: '卵料理'},
    { name: '定番おかず'},
@@ -34,6 +34,21 @@ Tag.create([
    { name: 'パン'},
    { name: '飲み物'},
    { name: '魚介料理'},
-   { name: '肉料理'},
-   { name: '麺類'}
    ])
+   
+customers = Customer.create!(
+  [
+    {last_name: '山田', first_name: '太郎', email: 'aaa@au.com', password: 'password'},
+    {last_name: '田中', first_name: '一郎', email: 'bbb@au.com', password: 'password'},
+    {last_name: '令和', first_name: '花子', email: 'ccc@au.com', password: 'password'},
+    {last_name: '平成', first_name: '莉子', email: 'ddd@au.com', password: 'password'},
+    {last_name: '深澤', first_name: '翔太', email: 'eee@au.com', password: 'password'},
+    {last_name: '岩本', first_name: '大介', email: 'fff@au.com', password: 'password'},
+    {last_name: '目黒', first_name: '亮平', email: 'ggg@au.com', password: 'password'},
+    {last_name: '向井', first_name: '辰哉', email: 'hhh@au.com', password: 'password'},
+    {last_name: '阿部', first_name: '康二', email: 'iii@au.com', password: 'password'},
+    {last_name: '宮舘', first_name: '由紀', email: 'jjj@au.com', password: 'password'},
+    {last_name: '渡辺', first_name: '眞帆', email: 'kkk@au.com', password: 'password'},
+    {last_name: '平野', first_name: '汐里', email: 'lll@au.com', password: 'password'}
+  ]
+)
